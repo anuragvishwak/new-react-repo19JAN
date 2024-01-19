cc581e1bcb18410b050e9725f587b082853483c4
anuragvishwak <87541383+anuragvishwak@users.noreply.github.com>
2024-01-19 14:21:25 +0530

    new file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import UsinguseState from './usestate';
+import UseNav from './useNav';
+import UseNavi from './useNav';
+import AApp from './useNav';
 
 function App() {
   return (
-    
     <div>
-  <UsinguseState/>
+      {/* <UsinguseState/> */}
+      <AApp/>
     </div>
   );
 }
-
 export default App;
--- src/usestate.js
+++ src/usestate.js
@@ -1,9 +1,12 @@
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState ,useRef} from 'react'
 
 function UsinguseState() {
     const [initial, setinitial] = useState(0)
     const [count, setcount] = useState(0)
+
+const ref = useRef()
+console.log(ref);
 
   useEffect(()=>{setcount(count+1)},[initial])
   return (
@@ -11,6 +14,8 @@ function UsinguseState() {
         <button onClick={()=>{setinitial(initial+1)}}>CLICK TO INCREASE</button>
         {initial}
         {count}
+        <input ref={ref}></input>
+        <button onClick={()=>ref.current.focus()}>click to focus</button>
     </div>
   )
 }
